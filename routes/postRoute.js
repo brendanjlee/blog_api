30d6e8d1fc4c70b6b601fb4f110fe9a02752eccb
Brendan <brendanjlee5@gmail.com>
2024-06-27 21:06:28 +0900

    Create comment controller

--- routes/postRoute.js
+++ routes/postRoute.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const { authenticateJWT } = require("../middleware/authMiddleware");
-const { postValidation } = require("../utils/validations");
+const { postValidation, commentValidation } = require("../utils/validations");
 
 const {
   getAllPosts,
@@ -33,10 +33,20 @@ router.delete("/:postId", authenticateJWT, deletePost);
 // Comment Routes
 router.get("/:postId/comments", getAllComments);
 
-router.post("/:postId/comments", createComment);
+router.post(
+  "/:postId/comments",
+  authenticateJWT,
+  commentValidation,
+  createComment
+);
 
-router.put("/:postId/comments/:commentId", updateComment);
+router.put(
+  "/:postId/comments/:commentId",
+  authenticateJWT,
+  commentValidation,
+  updateComment
+);
 
-router.delete("/:postId/comments/:commentId", deleteComment);
+router.delete("/:postId/comments/:commentId", authenticateJWT, deleteComment);
 
 module.exports = router;
---
+++ services/commentService.js
@@ -0,0 +1,35 @@
+const Comment = require("../models/Comment");
+
+const getAllComments = async (postId) => {
+  const comments = await Comment.find({ parentPost: postId })
+    .sort({ postedDate: -1 })
+    .exec();
+
+  return comments;
+};
+
+const createComment = async (newComment) => {
+  newComment.save();
+  return newComment;
+};
+
+const updateComment = async (commentId, updateData) => {
+  const comment = await Comment.findByIdAndUpdate(commentId, updateData, {
+    new: true,
+    runValidators: true,
+  });
+
+  return comment;
+};
+
+const deleteComment = async (commentId) => {
+  const result = await Comment.findByIdAndDelete(commentId);
+  return result;
+};
+
+module.exports = {
+  getAllComments,
+  createComment,
+  updateComment,
+  deleteComment,
+};
